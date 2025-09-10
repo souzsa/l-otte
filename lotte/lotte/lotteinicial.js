a571faadf628c204ca166359ba5bfd36644de2de
souzsa <130611983+souzsa@users.noreply.github.com>
2025-09-10 09:04:21 -0300

    Delete lotte directory

--- lotte/lotte/lotteinicial.js
+++
@@ -1,7 +0,0 @@
-// Script do menu mobile
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector("nav ul");
-
-hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
-});
